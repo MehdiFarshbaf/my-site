cc8a9083a8d0f3ca90432b256eb5ea7419f0fdb6
MehdiFarshbaf <mfarshbaf92@yahoo.com>
2024-04-12 13:46:50 +0330

    my skills

--- src/components/Home/MySkills/MySkills.jsx
+++ src/components/Home/MySkills/MySkills.jsx
@@ -1,8 +1,31 @@
 import Section from "../../Section/Section";
+import Chart from "../../Chart/Chart";
+import {skillsList} from "../../../helper/data";
+
+//styles
+import styles from './MySkills.module.scss'
+import {FaArrowRight} from "react-icons/fa";
+
 
 const MySkills = () => {
     return (
-        <Section title="مهارت های من" subTitle="چیزی که میدانم"></Section>
+        <Section title="مهارت های من" subTitle="چیزی که میدانم">
+            <div className={styles.content}>
+
+                <div className={styles.chart}>
+                    {skillsList.map(skill => (
+                        <Chart info={skill}/>
+                    ))}
+                </div>
+                <div className={styles.description}>
+                    <h4 className='sub-title'>مهارت ها و تجربیات من</h4>
+                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab at
+                        cupiditate deleniti deserunt doloremque doloribus ducimus est laudantium libero magnam maxime
+                        perferendis quis quos repellendus reprehenderit, sequi vero voluptas?</p>
+                    <button className='button'>Read more <FaArrowRight/></button>
+                </div>
+            </div>
+        </Section>
     )
 }
 export default MySkills
--- src/helper/data.js
+++ src/helper/data.js
@@ -20,3 +20,25 @@ export const myServices=[
         icon: <FaCode className='icon'/>
     }
 ]
+export const skillsList = [
+    {
+        id: 0,
+        title: 'HTML',
+        percent: 90
+    }, {
+        id: 1,
+        title: 'CSS',
+        percent: 60
+    }, {
+        id: 2,
+        title: 'JavaScript',
+        percent: 80
+    }, {
+        id: 3,
+        title: 'PHP',
+        percent: 50
+    }, {
+        id: 4,
+        title: 'MySQL',
+        percent: 70
+    }]
